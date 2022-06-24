e8d17cb28225694b7311f8b83e0ea5884e450995
Jesse Schoch <ni8rjlf3l@js.brng.us>
2022-06-24 13:11:48 -0700

    Update with_cq_gears.py

--- with_cq_gears.py
+++ with_cq_gears.py
@@ -13,25 +13,34 @@ from cq_gears import SpurGear
 
 ##################################################################
 #stuff you should edit
-gear_module = 1.0
-tooth_count = 40
-pressure_angle = 18.0
+gear_module = 0.5
+tooth_count = 36
+pressure_angle = 20.0
 # more steps == finer but slower
-steps =50
+steps =14
 # render cuts 1 == true 0 == false
-render = 1
+render = 0
 # generate gcode boolean
 gcode = 1
 
+# How far you want to mill in x (the width of your tooth or blank)
+x_feed = 4
+
 # number of teeth holes to cut.  2 for full tooth
-render_tooth_count = 2
+render_tooth_count = 36
 
 # for gcode generation to drive a axis
-s_mm = 252.0
+
+s_mm = 31.7
+# factor is steps per mm / 360 to get steps per degree factor
 factor = s_mm/360
 
 # additional depth for cut
 clearance = 1.
+
+filename = "out.nc"
+
+# TODO: need to add preamble and post to turn spindle on etc
 
 ##################################################################
 # stuff you shouldn't edit
@@ -46,9 +55,11 @@ wp = (
         .transformed(offset=(0,0, 1.0), rotate=(0, 0, rotate_compare))
         .gear(spur_gear))
 
+gcodes = []
+
 tooth_height = 2.4 * gear_module
 
-if gear_module < 1.25:
+if gear_module > 1.25:
     tooth_height = 2.25 * gear_module
 
 pitch_diameter = tooth_count * gear_module
@@ -59,13 +70,15 @@ print(f'blank_diameter {blank_diameter}')
 
 
 
-travel_distance_x = (pitch_diameter*.9)
+travel_distance_x = (pitch_diameter*.55)
 old_y_pos = -(travel_distance_x/2)
 
 y_pos = old_y_pos
 step_linear = travel_distance_x / steps
 
-step_degree = (360)/ ((pitch_diameter * math.pi )/step_linear)
+print(f'pitch_diameter = {pitch_diameter} ')
+
+step_degree = 360 / ((pitch_diameter * math.pi )/step_linear)
 print(f'Step Linear X {step_linear} Step Degree {step_degree}')
 
 
@@ -76,7 +89,7 @@ tooth_pitch_spacing = circular_pitch/2
 
 addendum = gear_module
 dedendum = tooth_height - addendum
-print(f'dedendum {dedendum} ')
+print(f'dedendum {dedendum} tooth_height {tooth_height} ')
 
 depth = tooth_height * clearance 
 
@@ -120,7 +133,7 @@ gear_tool = (
         )
 
 
-y_feed = 5
+
 
 
 
@@ -135,8 +148,9 @@ def cut_tooth():
     global gear_tool
     global render
     global gcode
-    global y_feed
+    global x_feed
     global factor
+    tmp_x_feed = x_feed
     for x in range(0,steps,1):        
         
         y_pos = y_pos + step_linear;
@@ -144,18 +158,20 @@ def cut_tooth():
             gear_tool = gear_tool.translate((0,step_linear,0))        
             gear_blank = gear_blank.rotate((0,0,0),(0,0,-1),-step_degree ).cut(gear_tool)
         if(gcode):
-            print(f'G91 Y{step_linear}')
-            print(f'G91 A{-step_degree * factor}')
-            print(f'G91 X{y_feed}')
-            y_feed = y_feed * -1
+            gcodes.append(f';step {x}')
+            gcodes.append(f'G91 Y{step_linear}')
+            gcodes.append(f'G91 A{-step_degree * factor}')
+            gcodes.append(f'G91 X{tmp_x_feed}')
+            tmp_x_feed = tmp_x_feed * -1
         
     #rotate back to beginning
     if(render):
         gear_blank = gear_blank.rotate((0,0,0),(0,0,-1),step_degree*steps )
     if(gcode):
-        print(f'G91 Z10')
-        print(f'G90 Y0X0')
-        print(f'G91 Z-10')
+        
+        gcodes.append(f'G91 Z10')
+        gcodes.append(f'G90 Y0X0')
+        gcodes.append(f'G91 Z-10')
     y_pos = old_y_pos
 
 
@@ -169,12 +185,23 @@ for tooth in range(render_tooth_count):
         #show_object(gear_tool)
         #show_object(gear_blank)
     if(gcode):
-        print(f'G91 A{(360/tooth_count)*factor}')
+        #  move a axis back to initial position
+        gcodes.append(f';tooth {tooth}')
+        # go to beginning
+        gcodes.append(f'G91 A{step_degree * steps*factor}')
+        # advance on tooth
+        gcodes.append(f'g91 A{(360/tooth_count) * factor}')
         #print(f'')
 
 if(gcode):
-    for x in range(10):
+    print('writing file')
+    text_file = open(filename, "w")
+    n = text_file.write('\n'.join(gcodes))
+    text_file.close()
+    #print('\n'.join(gcodes))
+    for x in range(3):
         print("\n")
 # Displays the result of this script
 
+print('done')
 #show_object(gear_blank)
